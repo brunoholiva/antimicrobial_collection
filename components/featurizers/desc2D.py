cd31452f0543d5b7e4c5fde538a025c374534be3
brunoholiva <brunodias1707@gmail.com>
2025-10-27 14:54:57 -0300

    feat: add logic to remove NaN producing descriptors

--- components/featurizers/desc2D.py
+++ components/featurizers/desc2D.py
@@ -2,8 +2,10 @@ import pandas as pd
 import argparse
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+
 # Assuming get_desc2d is in src.chemical_representations
 from src.chemical_representations import get_desc2d
+
 
 def main(args):
     """
@@ -16,7 +18,7 @@ def main(args):
     for smiles in train_df[args.smiles_col]:
         descriptor = get_desc2d(smiles)
         train_descriptors_list.append(descriptor)
-    
+
     desc_cols = [f"desc2d_{i}" for i in range(train_descriptors_list[0].shape[0])]
     train_desc_df = pd.DataFrame(train_descriptors_list, columns=desc_cols)
 
@@ -26,9 +28,17 @@ def main(args):
         test_descriptors_list.append(descriptor)
     test_desc_df = pd.DataFrame(test_descriptors_list, columns=desc_cols)
 
+    # drop any columns with NaNs
+    train_desc_df = train_desc_df.dropna(axis=1)
+    test_desc_df = test_desc_df[train_desc_df.columns]
+    test_desc_df = test_desc_df.dropna(axis=1)
+    train_desc_df = train_desc_df[test_desc_df.columns]
+
+    desc_cols = train_desc_df.columns
+
     scaler = StandardScaler()
     train_desc_scaled = scaler.fit_transform(train_desc_df)
-    test_desc_scaled = scaler.transform(test_desc_df) 
+    test_desc_scaled = scaler.transform(test_desc_df)
 
     train_desc_scaled_df = pd.DataFrame(train_desc_scaled, columns=desc_cols)
     test_desc_scaled_df = pd.DataFrame(test_desc_scaled, columns=desc_cols)
@@ -43,13 +53,23 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="2D Descriptor Featurizer with Normalization")
-    
-    parser.add_argument("--train_csv", type=str, help="Path to the input training CSV file.")
+    parser = argparse.ArgumentParser(
+        description="2D Descriptor Featurizer with Normalization"
+    )
+
+    parser.add_argument(
+        "--train_csv", type=str, help="Path to the input training CSV file."
+    )
     parser.add_argument("--test_csv", type=str, help="Path to the input test CSV file.")
-    parser.add_argument("--train_output_csv", type=str, help="Path to save the output training features.")
-    parser.add_argument("--test_output_csv", type=str, help="Path to save the output test features.")
-    
+    parser.add_argument(
+        "--train_output_csv",
+        type=str,
+        help="Path to save the output training features.",
+    )
+    parser.add_argument(
+        "--test_output_csv", type=str, help="Path to save the output test features."
+    )
+
     parser.add_argument(
         "--smiles_col",
         type=str,
